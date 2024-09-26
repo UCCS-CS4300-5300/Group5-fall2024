6e706242ad478be01bce155eb9ef29ac7c0a8cc3
root <Naomi>
2024-09-26 22:05:42 +0000

    base_template changes

--- Chit-Chatty/myproject/myproject/settings.py
+++ Chit-Chatty/myproject/myproject/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
