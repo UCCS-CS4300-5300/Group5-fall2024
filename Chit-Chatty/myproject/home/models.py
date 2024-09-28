2f615718c2c377db50e965bce5db0d9baa82c586
Darion <dbadillo@uccs.edu>
2024-09-28 05:05:06 +0000

    Home page Next quiz implemented. New model Quiz. Made migrations

--- Chit-Chatty/myproject/home/models.py
+++ Chit-Chatty/myproject/home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+# models.py
+from django.db import models
+
+
+# Quiz model
+class Quiz(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    url = models.CharField(max_length=100)
+    # Flag to identify the next quiz
+    is_next = models.BooleanField(default=False)  
+
+    def __str__(self):
+        return self.title
--- Chit-Chatty/myproject/home/views.py
+++ Chit-Chatty/myproject/home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Quiz
 
-# Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
-    # return HttpResponse('home page')
+    # Fetch the quiz that is marked as the next quiz
+    next_quiz = Quiz.objects.filter(is_next=True).first()
+    
+    context = {
+        'quiz_title': next_quiz.title if next_quiz else 'No Quiz Available',
+        'quiz_description': next_quiz.description if next_quiz else 'Check back later for new content.',
+        'quiz_url': next_quiz.url if next_quiz else '#',
+    }
+    return render(request, 'home/index.html', context)
