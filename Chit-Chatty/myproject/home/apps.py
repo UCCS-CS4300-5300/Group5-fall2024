b459de2733d4b8b55ade73389e9dec623ec9127e
Darion <dbadillo@uccs.edu>
2024-12-03 03:44:27 +0000

    Smells Fixed: home/admin.py, home/apps.py

--- Chit-Chatty/myproject/home/apps.py
+++ Chit-Chatty/myproject/home/apps.py
@@ -1,12 +1,14 @@
 from django.apps import AppConfig
 import os
 
+
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
-    
+
     def ready(self):
-        # Checks if the current process is the main one. If so, run the scheduler
+        # Checks if the current process is the main one. 
+        # If so, run the scheduler
         if os.environ.get('RUN_MAIN', None) == 'true':
             from .scheduler import startScheduler
             startScheduler()
--- Chit-Chatty/myproject/myproject/settings.py
+++ Chit-Chatty/myproject/myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,8 +145,6 @@ CSRF_TRUSTED_ORIGINS = [
     'https://app-dbadillo-5.devedu.io',
     'https://app-cromo-5.devedu.io',
 ]
-
-from decouple import config
 
 OPENAI_API_KEY = config('OPENAI_API_KEY')
 
