854ffd208bb893e073b47eacefca4d78fc9af0bf
Darion <dbadillo@uccs.edu>
2024-12-03 04:06:43 +0000

    Smells Fixed: home/models.py, home/scheduler.py

--- Chit-Chatty/myproject/home/models.py
+++ Chit-Chatty/myproject/home/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.translation import gettext_lazy as _
+# from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
@@ -11,18 +11,24 @@ class Quiz(models.Model):
     description = models.TextField()
     length = models.IntegerField(default=5)
     difficulty = models.CharField(max_length=20, default="Easy")
-    
+
     # Link quiz to user (optional, uncomment to use)
-    user = models.ForeignKey('Member', on_delete=models.CASCADE, null=True, blank=True)
-    
+    user = models.ForeignKey('Member', on_delete=models.CASCADE,
+                             null=True, blank=True)
+
     # Holds all questions in the model
-    questions = models.ManyToManyField('Question', related_name='quiz_questions')
+    questions = models.ManyToManyField('Question',
+                                       related_name='quiz_questions')
 
     # For quiz tracking
     is_active = models.BooleanField(default=True)
     is_completed = models.BooleanField(default=False)
     score = models.IntegerField(null=True, blank=True)
-    curr_question = models.ForeignKey('Question', on_delete=models.SET_NULL, null=True, blank=True)
+    curr_question = models.ForeignKey(
+                                      'Question',
+                                      on_delete=models.SET_NULL,
+                                      null=True,
+                                      blank=True)
     correct_count = models.IntegerField(default=0)
     incorrect_count = models.IntegerField(default=0)
 
@@ -33,37 +39,39 @@ class Quiz(models.Model):
 # Class model
 class Question(models.Model):
     # Phrase in the source language
-    translation_question = models.CharField(max_length=300)  
-    correct_answer = models.CharField(max_length=300)  
-    source_language = models.CharField(max_length=50, default="Spanish")  
-    target_language = models.CharField(max_length=50, default="English")  
+    translation_question = models.CharField(max_length=300)
+    correct_answer = models.CharField(max_length=300)
+    source_language = models.CharField(max_length=50, default="Spanish")
+    target_language = models.CharField(max_length=50, default="English")
 
     def __str__(self):
         return self.translation_question
 
+
 # User model
 class Member(models.Model):
     # Fields that makes a user unique
-    userName = models.CharField(max_length= 50)
-    firstName = models.CharField(max_length= 50, default="")
-    lastName = models.CharField(max_length= 50, default="")
-    email = models.CharField(max_length= 50, default="")
+    userName = models.CharField(max_length=50)
+    firstName = models.CharField(max_length=50, default="")
+    lastName = models.CharField(max_length=50, default="")
+    email = models.CharField(max_length=50, default="")
     dateJoined = models.DateField(auto_now_add=True)
 
-    # Streak information 
-    streakCount = models.IntegerField(default = 0)
-    longestStreak = models.IntegerField(default = 0)
-    hasCompletedQuiz = models.BooleanField(default = False)
+    # Streak information
+    streakCount = models.IntegerField(default=0)
+    longestStreak = models.IntegerField(default=0)
+    hasCompletedQuiz = models.BooleanField(default=False)
 
     # One to one relationship with user
-    user = models.OneToOneField(User, null= True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.userName
 
+
 # Tracks last reset for streaks (since our servers aren't always running)
 class LastStreakReset(models.Model):
-    lastReset = models.DateTimeField(auto_now = True)
+    lastReset = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Last streak reset was at: {self.lastReset}"
--- Chit-Chatty/myproject/home/scheduler.py
+++ Chit-Chatty/myproject/home/scheduler.py
@@ -1,7 +1,7 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
-from django_apscheduler.jobstores import DjangoJobStore
-from .tasks import resetStreak 
+# from django_apscheduler.jobstores import DjangoJobStore
+from .tasks import resetStreak
 from .models import LastStreakReset
 from django.utils import timezone
 
@@ -15,25 +15,28 @@ def startScheduler():
     scheduler.add_job(
         resetStreak,
         trigger=CronTrigger(hour=0, minute=0),
-        #trigger=CronTrigger(minute='*'),  RUNS EVERY MINUTE. USE FOR TESTING
+        # trigger=CronTrigger(minute='*'),  RUNS EVERY MINUTE. USE FOR TESTING
         id="reset_streak_job",
         replace_existing=True,
     )
 
-    ''' 
-    This section for forcing a reset is only here because our server doesn't run 24/7
     '''
-    # Get the last time when streak was reset 
+    Forces reset since server doesn't run 24/7
+    '''
+    # Get the last time when streak was reset
     lastResetTime, created = LastStreakReset.objects.get_or_create()
 
     # Check if the current date is different from the last execution date
     currentTime = timezone.now()
-    
-    # If the current date is after the last run date, run the resetStreak function
+
+    # If the current date is after the last run date, resetStreak
     if currentTime.date() > lastResetTime.lastReset.date():
-        resetStreak()  # Run the function to reset the streak
-        lastResetTime.lastReset = currentTime  # Update the last run time
-        lastResetTime.save()  # Save the log entry
+        # Run the function to reset the streak
+        resetStreak()
+        # Update the last run time
+        lastResetTime.lastReset = currentTime
+        # Save the log entry
+        lastResetTime.save()
 
     # Start the scheduler to being running the scheduled jobs in the background
     scheduler.start()
